58642a27bf29f6bb42d63b97be9ea74abbffffb7
rafal <rafal.szponarski@o2.pl>
2024-06-09 12:47:56 +0200

    3rd

---
+++ aliases - parsing data from web/mWIG40_data_clean_manually.py
@@ -0,0 +1,16 @@
+def delete_lines_and_modify_sa(input_file_name, output_file_name):
+    with open(input_file_name, 'r') as input_file:
+        lines = input_file.readlines()
+
+    linie_for_save = [lin.replace('SA', 'S.A.') if 'SA' in lin else lin for lin in lines if any(c.isalpha() for c in lin.strip())]
+
+    with open(output_file_name, 'w') as output_file:
+        for n_lin in linie_for_save:
+            output_file.write(n_lin)
+
+
+my_file_in = "wig_40_file_to_clean.txt"
+my_file_out = "wig_40_cleanup.txt"
+
+delete_lines_and_modify_sa(my_file_in, my_file_out)
+print("File is ready")
